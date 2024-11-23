2c31bb37489dd1aa653afa579de12cc36eb16651
Collin Chiang <collin.chiang1@gmail.com>
2024-11-23 13:27:39 -0500

    renamed message guild attribute as guild_id

--- server/app/models/message.py
+++ server/app/models/message.py
@@ -8,7 +8,7 @@ class Message(Base):
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True)
-    guild = Column(Integer, ForeignKey("guilds.id"))
+    guild_id = Column(Integer, ForeignKey("guilds.id"))
     content = Column(String)
     embedding = Column(Vector(4096))
     timestamp = Column(DateTime)

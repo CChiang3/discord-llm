9468de9e102bfdf6da74dd683a7f783d30124ba8
Collin Chiang <collin.chiang1@gmail.com>
2024-11-26 17:00:35 -0500

    added basic status codes to accompany the responses

--- server/app/routers/guild_router.py
+++ server/app/routers/guild_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 
 from app.database.session import get_session
@@ -9,13 +9,30 @@ router = APIRouter(prefix="/guilds")
 
 
 @router.post("/", response_model=GuildCreate)
-async def create_guild(guild: GuildCreate, session: Session = Depends(get_session)):
+async def create_guild(
+    guild: GuildCreate, response: Response, session: Session = Depends(get_session)
+):
     service = GuildService(session)
-    await service.create_guild(guild)
+
+    try:
+        await service.create_guild(guild)
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+
+    response.status_code = status.HTTP_201_CREATED
     return guild
 
 
 @router.delete("/{guild_id}")
-async def delete_guild(guild_id: int, session: Session = Depends(get_session)):
+async def delete_guild(guild_id: int, response: Response, session: Session = Depends(get_session)):
     service = GuildService(session)
-    return await service.delete_guild(guild_id)
+
+    try:
+        await service.delete_guild(guild_id)
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+
+    response.status_code = status.HTTP_204_NO_CONTENT
+    return
--- server/app/routers/message_router.py
+++ server/app/routers/message_router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response, status
 from sqlalchemy.orm import Session
 
 from app.database.session import get_session
@@ -9,13 +9,31 @@ router = APIRouter(prefix="/messages")
 
 
 @router.post("/", response_model=MessageCreate)
-async def create_message(message: MessageCreate, session: Session = Depends(get_session)):
+async def create_message(
+    message: MessageCreate, response: Response, session: Session = Depends(get_session)
+):
     service = MessageService(session)
-    await service.create_message(message)
+    try:
+        await service.create_message(message)
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        print("erro")
+        return
+
+    response.status_code = status.HTTP_201_CREATED
     return message
 
 
 @router.delete("/{message_id}")
-async def delete_message(message_id: int, session: Session = Depends(get_session)):
+async def delete_message(
+    message_id: int, response: Response, session: Session = Depends(get_session)
+):
     service = MessageService(session)
-    return await service.delete_message(message_id)
+    try:
+        await service.delete_message(message_id)
+    except Exception:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+        return
+
+    response.status_code = status.HTTP_204_NO_CONTENT
+    return

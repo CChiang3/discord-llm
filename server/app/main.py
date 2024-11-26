8a87ae8be1fea8bf6d8263f83a7b8427c8887b40
Collin Chiang <collin.chiang1@gmail.com>
2024-11-26 14:58:38 -0500

    refactored graphql schema to graphql

--- server/app/main.py
+++ server/app/main.py
@@ -2,8 +2,8 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
 from app.database import Base, get_engine
+from app.graphql import schema
 from app.models import Guild, Message  # noqa: F401
-from app.schema import schema
 
 engine = get_engine()
 Base.metadata.create_all(bind=engine)
